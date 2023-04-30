b3da93bd85da56cbecbb700e044c8ee12ea84f2f
まつ <92625267+matsukz@users.noreply.github.com>
2023-04-30 20:07:33 +0900

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.IO
+Imports Newtonsoft.Json.Linq
+
+Module Module1
+
+    Sub Main()
+
+        ' Enter Path
+        Dim JsonPath As String = ""
+        Dim JsonString As String = File.ReadAllText(JsonPath)
+        Dim jObject As JObject = JObject.Parse(JsonString)
+
+        ' Enter Key
+        Dim ID As String = jObject("")("").ToString()
+
+        System.Console.WriteLine(ID)
+        System.Console.ReadLine()
+
+    End Sub
+
+End Module
